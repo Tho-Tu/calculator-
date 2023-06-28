48d8db5ad6a5e614aa113dbd2a66ebea0c8a32f0
Tho Tu <126043463+Tho-Tu@users.noreply.github.com>
2023-06-28 17:11:34 +1000

    Add button functionality for 2 operands

--- script.js
+++ script.js
@@ -1,6 +1,155 @@
-let firstNumber = null;
-let secondNumber = null;
-let operator = null;
+let globalNumber = '';
+let firstNumber = '';
+let secondNumber = '';
+let operator = '';
+let answerNumber = null;
+let isFirstNumber = true;
+let isDecimal = false;
+
+function display(globalNumber){
+    if (isFirstNumber === true){
+        return firstNumber = globalNumber;
+    }
+    else if (isFirstNumber === false){
+        return secondNumber = globalNumber; 
+        }
+}
+
+function reset(){
+    globalNumber = '';
+    firstNumber = '';
+    secondNumber = '';
+    operator = '';
+    answerNumber = null;
+    isFirstNumber = true;
+    }
+
+function operatorReset(){
+    isFirstNumber = false;
+    isDecimal = false;
+    globalNumber = '';
+}
+
+const displayScreen = document.querySelector('.screen');
+
+const clearButton = document.querySelector('#clear');
+clearButton.addEventListener('click', () => {
+    displayScreen.textContent = '';
+    reset();
+});
+
+const deleteButton = document.querySelector('#delete');
+deleteButton.addEventListener('click', () => {
+    globalNumber = globalNumber.substring(0, globalNumber.length - 1);
+    displayScreen.textContent = display(globalNumber);
+});
+
+const sevenButton = document.querySelector('#seven');
+sevenButton.addEventListener('click', () => {
+    globalNumber += '7';
+    displayScreen.textContent = display(globalNumber);
+});
+
+const eightButton = document.querySelector('#eight');
+eightButton.addEventListener('click', () => {
+    globalNumber += '8';
+    displayScreen.textContent = display(globalNumber);
+});
+
+const nineButton = document.querySelector('#nine');
+nineButton.addEventListener('click', () => {
+    globalNumber += '9';
+    displayScreen.textContent = display(globalNumber);
+});
+
+const divideButton = document.querySelector('#divide');
+divideButton.addEventListener('click', () => {
+    displayScreen.textContent = '/';
+    operator = '/';
+    operatorReset();
+});
+
+const fourButton = document.querySelector('#four');
+fourButton.addEventListener('click', () => {
+    globalNumber += '4';
+    displayScreen.textContent = display(globalNumber);
+});
+
+const fiveButton = document.querySelector('#five');
+fiveButton.addEventListener('click', () => {
+    globalNumber += '5';
+    displayScreen.textContent = display(globalNumber);
+});
+
+const sixButton = document.querySelector('#six');
+sixButton.addEventListener('click', () => {
+    globalNumber += '6';
+    displayScreen.textContent = display(globalNumber);
+});
+
+const multiplyButton = document.querySelector('#multiply');
+multiplyButton.addEventListener('click', () => {
+    displayScreen.textContent = '*';
+    operator = '*';
+    operatorReset();
+});
+
+const oneButton = document.querySelector('#one');
+oneButton.addEventListener('click', () => {
+    globalNumber += '1';
+    displayScreen.textContent = display(globalNumber);
+});
+
+const twoButton = document.querySelector('#two');
+twoButton.addEventListener('click', () => {
+    globalNumber += '2';
+    displayScreen.textContent = display(globalNumber);
+});
+
+const threeButton = document.querySelector('#three');
+threeButton.addEventListener('click', () => {
+    globalNumber += '3';
+    displayScreen.textContent = display(globalNumber);
+});
+
+const minusButton = document.querySelector('#minus');
+minusButton.addEventListener('click', () => {
+    displayScreen.textContent = '-';
+    operator = '-';
+    operatorReset();
+});
+
+const decimalButton = document.querySelector('#decimal');
+decimalButton.addEventListener('click', () => {
+    if (isDecimal === false) {
+        globalNumber += '.';
+        displayScreen.textContent = display(globalNumber);
+        isDecimal = true;
+    }
+});
+
+const zeroButton = document.querySelector('#zero');
+zeroButton.addEventListener('click', () => {
+    globalNumber += '0';
+    displayScreen.textContent = display(globalNumber);
+});
+
+const equalsButton = document.querySelector('#equals');
+equalsButton.addEventListener('click', () => {
+    displayScreen.textContent = display(operate(operator, 
+        firstNumber, secondNumber));
+    isFirstNumber = false;
+    isDecimal = false;
+    firstNumber = operate(operator, firstNumber, secondNumber);
+});
+
+const addButton = document.querySelector('#add');
+addButton.addEventListener('click', () => {
+    displayScreen.textContent = '+';
+    operator = '+';
+    operatorReset();
+});
+
 
 function add(a, b){
     return Math.round((a+b)*100)/100;
@@ -15,22 +164,26 @@ function multiply(a, b){
 }
 
 function divide(a, b){
+    if (b === 0){
+        return 'bruh';
+    }
     return Math.round((a/b)*100)/100;
 }
 
-function operate(firstNumber, secondNumber, operator){
+function operate(operator, firstNumber, secondNumber){
+    if (operator === null){
+        return;
+    }
+    firstNumber = Number(firstNumber);
+    secondNumber = Number(secondNumber);
     switch(operator) {
         case '+':
-            add(firstNumber,secondNumber);
-            break;
+            return answerNumber = add(firstNumber,secondNumber);
         case '-':
-            minus(firstNumber,secondNumber);
-            break;
+            return answerNumber = minus(firstNumber,secondNumber);
         case '*':
-            multiply(firstNumber,secondNumber);
-            break;
+            return answerNumber = multiply(firstNumber,secondNumber);
         case '/':
-            divide(firstNumber,secondNumber);
-            break;
+            return answerNumber = divide(firstNumber,secondNumber);
     }
 }
